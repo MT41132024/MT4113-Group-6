06681e442ca71e51edc72a25fd8c11f0298b630a
dphmusto <dphmusto@gmail.com>
2025-11-14 12:55:40 +0000

    Reformatted functions, added minimum condition

--- testing.R
+++ testing.R
@@ -2,41 +2,55 @@
 library(numDeriv)
 
 # Univariate Newton Method
-UVN <- function(start, f, data, epsilon){
-  beta <- start
+UVN <- function(f, inits, data, minimum=TRUE, tol=1e-5){
+  theta <- inits
   delta <- 10^3
-  while (abs(delta/beta) > epsilon){
-    g <- grad(f, beta, data = data)
-    h <- hessian(f, beta, data = data)[1,1]
+  
+  if (minimum == FALSE){
+    f <- -f
+  }
+  
+  while (abs(delta/theta) > tol){
+    g <- grad(f, theta, data = data)
+    h <- hessian(f, theta, data = data)[1,1]
     
     delta <- -g/h
-    beta <- beta + delta
+    theta <- theta + delta
   }
-  return(beta)
+  return(theta)
 }
 
 # Multivariate Newton Method
-MVN <- function(start, f, data, epsilon){
-  beta <- start
+MVN <- function(f, inits, data, minimum=TRUE, tol=1e-5){
+  theta <- inits
   delta <- 10^3
+  
+  if (minimum == FALSE){
+    f <- -f
+  }
+  
   # For each step, calculate gradient, hessian, solve linear system for delta
-  while (min(abs(delta/beta)) > epsilon){ # Take minimum abs. change in each 
-    g <- grad(f, beta, data = data)       # par, not total step size
-    H <- hessian(f, beta, data = data)
+  while (min(abs(delta/theta)) > tol){ # Take minimum abs. change in each 
+    g <- grad(f, theta, data = data)       # par, not total step size
+    H <- hessian(f, theta, data = data)
     
     delta <- solve(H, -g)
     
-    beta <- beta + delta
+    theta <- theta + delta
   }
-  return(beta)
+  return(theta)
 }
 
 # Gauss-Newton Method (nls)
-GN <- function(start, f, data, epsilon){
-  theta <- start
+GN <- function(f, inits, data, minimum=TRUE, tol=1e-5){
+  theta <- inits
   delta <- 10^3
   
-  while (min(abs(delta/theta)) > epsilon) {
+  if (minimum == FALSE){
+    f <- -f
+  }
+  
+  while (min(abs(delta/theta)) > tol) {
     j <- jacobian(function(x, data) data$y - f(data$x, x), theta, data = data)
     r <- data$y - f(data$x, theta)
     
